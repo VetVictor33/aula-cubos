011e5a22d53ba137485934b1a8489f8c95a5422f
Victor Feliciano <victorjfeliciano@gmail.com>
2022-11-04 17:17:30 -0300

    Arrumei

--- olaMundo.js
+++ olaMundo.js
@@ -1,2 +1,3 @@
 console.log("Aqui é RJ");
-console.log("Não está funcionando este código por algum motivo conspicuoso");
+console.log("Não está funcionando este código por algum motivo não conspícuo");
+console.log("Precisa usar o comando node pra rodar. Bem notado");
