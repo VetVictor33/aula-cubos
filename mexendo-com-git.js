f7c17d4100a35a75a6e9f858e11a1a1ab9ae272b
Victor Feliciano <victorjfeliciano@gmail.com>
2022-11-07 17:56:53 -0300

    Comitando o arquivo após add, primeira mudança

--- mexendo-com-git.js
+++ mexendo-com-git.js
@@ -1,1 +1,6 @@
 //Arquivo criado no repositório remoto
+
+//Parte do arquivo adicionada no repositório local
+
+console.log("Quero estudar javaScript");
+console.log("Tentei usar printf('') pra escrever no console... isso que dá estudar duas linguagens ao mesmo tempo, no caso C");
