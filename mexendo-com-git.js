694e63a16ff7e2b41447a1b2702799c00e1fcb34
Victor Feliciano <victorjfeliciano@gmail.com>
2022-11-07 18:08:56 -0300

    Primeira modificação da ranch hml

--- mexendo-com-git.js
+++ mexendo-com-git.js
@@ -3,4 +3,6 @@
 //Parte do arquivo adicionada no repositório local
 
 console.log("Quero estudar javaScript");
+console.log("Trecho de linha adicionado na nova branch 'hml' ");
 console.log("Tentei usar printf('') pra escrever no console... isso que dá estudar duas linguagens ao mesmo tempo, no caso C");
+console.log("Também botei isso na branch hml");
